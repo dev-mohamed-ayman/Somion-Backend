3708aba33c6ffe7c26dfd83e1c5e1fe8c8414acc
Mohamed Ayman <devel.mohamed.ayman@gmail.com>
2024-09-16 17:04:27 +0300

    update

--- app/Http/Middleware/CorMiddleware.php
+++ app/Http/Middleware/CorMiddleware.php
@@ -18,7 +18,7 @@ class CorMiddleware
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
     }
 }
