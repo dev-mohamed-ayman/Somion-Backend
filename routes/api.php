c731224fba72482fc5fa3a116d96f633e8e4bb1b
Mohamed Ayman <devel.mohamed.ayman@gmail.com>
2024-09-16 17:18:36 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
+use Illuminate\Http\Middleware\HandleCors;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Admin Routes
-Route::prefix('admin')->middleware()->group(function () {
+Route::prefix('admin')->middleware([HandleCors::class])->group(function () {
     include 'admin.php';
 });
