dc9d061d0ed6e68ed3f659afaa9abc19d17d4a95
Mohamed Ayman <devel.mohamed.ayman@gmail.com>
2024-10-29 16:17:04 +0300

    more updates

--- app/Http/Controllers/Website/WebsiteController.php
+++ app/Http/Controllers/Website/WebsiteController.php
@@ -22,70 +22,96 @@ use Illuminate\Http\Request;
 
 class WebsiteController extends Controller
 {
-    public function index()
+    public function hero()
     {
         $hero = [
             'section' => Hero::query()->select('title', 'short_description', 'btn_title')->first(),
             'items' => HeroItem::query()->limit(5)->get()
         ];
 
+        return apiResponse(true, 200, $hero);
+    }
+
+    public function services()
+    {
         $services = [
             'section' => ServiceSection::query()->select('title')->first(),
             'services' => ServiceCategory::query()
-                ->select('id', 'title', 'main_title')
+                ->select('id', 'title')
                 ->with(['services' => function ($service) {
-                    $service->select('id', 'title', 'main_title', 'sub_title', 'short_description', 'description', 'image', 'main_image', 'service_category_id');
+                    $service->select('id', 'title', 'service_category_id');
                 }])
                 ->get()
         ];
 
+        return apiResponse(true, 200, $services);
+    }
+
+    public function projects()
+    {
         $projects = [
             'section' => HomeProjectSection::query()->select('title', 'sub_title', 'description')->first(),
             'projects' => HomeProject::query()
                 ->select('id', 'title', 'description', 'link')
                 ->with([
                     'categories' => function ($category) {
-                        $category->select('id', 'name');
+                        $category->select('home_project_categories.id', 'home_project_categories.name');
                     },
                     'images' => function ($image) {
-                        $image->select('id', 'path');
+                        $image->select('home_project_images.path', 'home_project_images.home_project_id');
                     }
                 ])
                 ->get()
         ];
 
+        return apiResponse(true, 200, $projects);
+    }
+
+    public function brands()
+    {
         $brands = [
             'section' => BrandSection::query()->select('title')->first(),
             'brands' => Brand::query()->select('image')->get()
         ];
+        return apiResponse(true, 200, $brands);
+    }
 
+    public function whies()
+    {
         $whies = [
             'section' => WhySection::query()->select('title')->first(),
             'whies' => Why::query()->select('title', 'description')->get()
         ];
+        return apiResponse(true, 200, $whies);
+    }
 
+    public function rates()
+    {
         $rates = [
             'section' => RateSection::query()->select('title', 'sub_title', 'paragraph')->first(),
             'rates' => Rate::query()->where('status', true)
                 ->select('first_name', 'last_name', 'image', 'message', 'rate')
                 ->get()
         ];
+        return apiResponse(true, 200, $rates);
+    }
 
+    public function startSection()
+    {
         $startSection = StartSection::query()->select('title', 'description', 'btn_title')->first();
+        return apiResponse(true, 200, $startSection);
+    }
 
+    public function footer()
+    {
         $footer = Footer::query()
             ->select('logo', 'emails', 'phone', 'whatsapp', 'instagram', 'linkedin', 'x', 'be', 'location', 'subscription_paragraph', 'copyright')
             ->first();
 
+        $serviceCategories = ServiceCategory::query()->select('id', 'title')->get();
         return apiResponse(true, 200, [
-            'hero' => $hero,
-            'services' => $services,
-            'projects' => $projects,
-            'brands' => $brands,
-            'whies' => $whies,
-            'rates' => $rates,
-            'startSection' => $startSection,
             'footer' => $footer,
+            'serviceCategories' => $serviceCategories,
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Website routes
-Route::get('home', [\App\Http\Controllers\Website\WebsiteController::class, 'index']);
-
+Route::controller(\App\Http\Controllers\Website\WebsiteController::class)->group(function () {
+    Route::get('hero', 'hero');
+    Route::get('services', 'services');
+    Route::get('projects', 'projects');
+    Route::get('brands', 'brands');
+    Route::get('whies', 'whies');
+    Route::get('rates', 'rates');
+    Route::get('start-section', 'startSection');
+    Route::get('footer', 'footer');
+});
 // Admin Routes
 Route::prefix('admin')->group(function () {
     include 'admin.php';
